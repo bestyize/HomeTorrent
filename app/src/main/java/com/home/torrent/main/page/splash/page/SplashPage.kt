d7572ecb3f8c01d7b17d39b45ab3ef86c72a3cea
bestyize <3011429848@qq.com>
2024-04-07 23:27:20 +0800

    add splash animation

--- app/src/main/java/com/home/torrent/main/page/splash/page/SplashPage.kt
+++ app/src/main/java/com/home/torrent/main/page/splash/page/SplashPage.kt
@@ -1,5 +1,7 @@
 package com.home.torrent.main.page.splash.page
 
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -8,28 +10,32 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.thewind.resources.R
 import com.home.torrent.ui.theme.BrandPink
 import com.home.torrent.ui.theme.TransportGray
+import com.thewind.resources.R
 import com.thewind.widget.ui.TitleHeader
 import kotlinx.coroutines.delay
 
@@ -43,13 +49,16 @@ import kotlinx.coroutines.delay
 @Preview
 fun SplashPage(onClose: () -> Unit = {}) {
 
+    val state = splashLogoAnimationStateHolder()
+
     val countDownState = remember {
         mutableStateOf(true)
     }
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.White).statusBarsPadding()
+            .background(Color.White)
+            .statusBarsPadding()
     ) {
         val timeLeft = remember {
             mutableIntStateOf(3)
@@ -85,7 +94,14 @@ fun SplashPage(onClose: () -> Unit = {}) {
             Image(
                 painter = painterResource(id = R.drawable.logo),
                 contentDescription = "",
-                modifier = Modifier.width(180.dp)
+                modifier = Modifier
+                    .size(state.logoSize.dp)
+                    .graphicsLayer {
+                        cameraDistance = 10000f
+                        rotationX = state.rotateAngle
+                        rotationZ = state.rotateAngle
+                        rotationY = state.rotateAngle
+                    }
             )
             TitleHeader(
                 stringResource(R.string.app_name),
@@ -129,3 +145,28 @@ private fun SkipButton(
         )
     }
 }
+
+@Composable
+private fun splashLogoAnimationStateHolder(): SplashLogoAnimationState {
+    var playRotate by remember {
+        mutableStateOf(false)
+    }
+
+    LaunchedEffect(key1 = Unit) {
+        playRotate = true
+    }
+    return SplashLogoAnimationState(
+        logoSize = animateFloatAsState(
+            targetValue = if (playRotate) 180f else 0f,
+            label = "logoSize",
+            animationSpec = tween(durationMillis = 1200)
+        ).value,
+        rotateAngle = animateFloatAsState(
+            targetValue = if (playRotate) 0f else 180f,
+            animationSpec = tween(durationMillis = 1200),
+            label = "logoRotate"
+        ).value
+    )
+}
+
+private data class SplashLogoAnimationState(val logoSize: Float = 0f, val rotateAngle: Float = 0f)
